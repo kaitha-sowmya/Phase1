9ca7ca44153ba7aec4662d9d5ad44238953c1ef3
sowmy <sowmy@LAPTOP-96QG03L3>
2022-01-13 11:27:32 +0530

    first commit

---
+++ Basics/src/Addition.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Add{
+	int res=0;
+	Add(int x,int y)
+	{
+	 res=x+y;
+	}
+}
+public class Addition {
+	
+	public static void main(String[] args) {
+		
+		
+			int a,b;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter first number");
+			a=sc.nextInt();
+			System.out.println("Enter second number");
+			b=sc.nextInt();
+			Add c=new Add(a,b);
+			System.out.println("Addition of two numbers is : "+c.res);
+		
+	}
+
+}
