2eaa511d9962f2b344fe23cbcb0f506665c463ca
sowmy <sowmy@LAPTOP-96QG03L3>
2022-01-13 11:40:15 +0530

    third commit

---
+++ Basics/src/print.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class print {
+	public static void main(String[] args) {
+		int a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");
+		a=sc.nextInt();
+		if(a>6) {
+		System.out.println("cheching");
+		}
+		else
+		{
+			System.out.println("enter again");
+		}
+	}
+
+}
