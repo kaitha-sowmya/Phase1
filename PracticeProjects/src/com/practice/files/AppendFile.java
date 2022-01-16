78c79c4e1001037cc8d75a71b5e7f36999a14a99
sowmy <sowmy@LAPTOP-96QG03L3>
2022-01-17 02:55:51 +0530

    fixbugs program commit one

--- PracticeProjects/src/com/practice/files/AppendFile.java
+++ PracticeProjects/src/com/practice/files/AppendFile.java
@@ -12,16 +12,7 @@ import java.io.BufferedWriter; import java.io.FileWriter; import java.io.IOExcep
 
 public class AppendFile {
 public static void main( String[] args ) {
-//	try {
-//		FileWriter writer = new FileWriter("D://filepath//testFile2.txt", true);
-//		writer.write("   appending to this file   ");
-//				writer.close();
-//				System.out.println("done");
-//		} 
-//		catch (IOException e) {
-//		e.printStackTrace();
-//		}
-//		}
+
 	try {
 		FileWriter f = new FileWriter("D://filepath//testFile3.txt", true); 
 		BufferedWriter b = new BufferedWriter(f);
